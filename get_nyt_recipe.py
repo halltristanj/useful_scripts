4e54cb8fcd52ebbde5b8369f6e984203757f6824
hall.tristanj <hall.tristanj@gmail.com>
2018-06-08 11:56:14 -0400

    Made fractions as fractions, not decimal

--- get_nyt_recipe.py
+++ get_nyt_recipe.py
@@ -11,9 +11,10 @@ args = vars(parser.parse_args())
 
 
 def remove_fract(text):
-    fraction = {8585: u'.0', 43056: u'.25', 43057: u'.5', 43058: u'.75', 43059: u'.0625', 43060: u'.125', 43061: u'.1875', 188: u'.25', 189: u'.5', 190: u'.75', 8528: u'.142857142857', 8529: u'.111111111111', 8531: u'.333333333333', 8532: u'.666666666667', 8533: u'.2', 8534: u'.4', 8535: u'.6', 8536: u'.8', 8537: u'.166666666667', 8538: u'.833333333333', 8539: u'.125', 8540: u'.375', 8541: u'.625', 8542: u'.875', 69245: u'.333333333333', 3443: u'.25', 3444: u'.5', 3445: u'.75', 69243: u'.5', 69244: u'.25', 11517: u'.5', 69246: u'.666666666667'}
+    fraction = {8585: u'.0', 43056: u'1/4', 43057: u'1/2', 43058: u'3/4', 43059: u'.0625', 43060: u'1/8', 43061: u'.1875', 188: u'1/4', 189: u'1/2', 190: u'3/4', 8528: u'.142857142857', 8529: u'.111111111111', 8531: u'1/3', 8532: u'2/3', 8533: u'1/5', 8534: u'2/5', 8535: u'3/5', 8536: u'4/5', 8537: u'.166666666667', 8538: u'.833333333333', 8539: u'1/8', 8540: u'.375', 8541: u'.625', 8542: u'.875', 69245: u'.333333333333', 3443: u'.25', 3444: u'.5', 3445: u'3/4', 69243: u'1/2', 69244: u'1/4', 11517: u'1/2', 69246: u'2/3'}
     text = text.translate(fraction)
-    parts = map(float, text.split())
+    # parts = map(float, text.split())
+    parts = text.split()
     return parts
 
 
@@ -38,7 +39,7 @@ for ingredient in ingredients:
     ingred = ingredient.find('span', attrs={'class': 'ingredient-name'}).text.strip()
     if quantity:
         quantity = str(list(remove_fract(quantity))[0]).rstrip('0').rstrip('.')
-        text = '- {0:>4} {1}'.format(quantity, ingred)
+        text = '- {0} {1}'.format(quantity, ingred)
     else:
         text = '-      {}'.format(ingred)
     text = textwrap.wrap(text, width=textwidth)
